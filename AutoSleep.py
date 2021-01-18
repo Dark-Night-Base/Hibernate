02add182f71202a4605b3ef439b6f98570fbd347
无风引漩 <scirocco_gti@yeah.net>
2021-01-18 23:54:57 +0800

    Create AutoSleep.py

---
+++ AutoSleep.py
@@ -0,0 +1,43 @@
+from mcdreforged.api.all import *
+import time
+
+PLUGIN_METADATA = {
+	'id': 'autosleep',
+	'version': '0.0.1',
+	'name': 'Auto Sleep',
+	'author': 'Sciroccogti',
+	'link': 'https://github.com/Dark-Night-Base/AutoSleep',
+}
+
+playerCount = 0
+
+# def on_info(server: ServerInterface, info: Info):
+
+
+def stop_server_later(server: ServerInterface, waitmin: int):
+    global playerCount
+    time.sleep(waitmin * 60)
+    if playerCount == 0:
+        server.say("Auto Sleep!")
+        server.stop()
+
+
+def on_load(server: ServerInterface, old_module):
+    # server.register_command(Literal('!!autosleep').then(Literal('help').runs()))
+    global playerCount
+    playerCount = 0
+    server.register_help_message(
+        "!!autosleep", "Sleep server at 10min after no one's online")
+
+
+def on_player_joined(server: ServerInterface, player: str, info: Info):
+    global playerCount
+    playerCount += 1
+
+
+def on_player_left(server: ServerInterface, player: str):
+    global playerCount
+    if playerCount > 0:
+        playerCount -= 1
+        if playerCount == 0:
+            stop_server_later(server, 10)
